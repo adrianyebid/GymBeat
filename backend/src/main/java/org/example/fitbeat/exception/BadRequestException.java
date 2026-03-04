64176f8efc4e49f300e09b6d78e994a82623d7e0
nicoarciniegas <nikolynchiano@gmail.com>
2026-03-04 12:11:09 -0500

    Delete java content

--- backend/src/main/java/org/example/fitbeat/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package org.example.fitbeat.exception;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
